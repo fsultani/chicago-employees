1c8c8c68b249a46984485610931d3117ac46c93a
fsultani <farid@Personal.local>
2018-07-29 16:31:56 -0700

    Add custom css to dropdown

--- src/Filter.js
+++ src/Filter.js
@@ -1,4 +1,44 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const Title = styled.div`
+  padding: 10px 0;
+  font-size: 18px;
+`
+
+const Container = styled.div`
+  padding: 0;
+  margin: 0 auto;
+  border: 1px solid #ccc;
+  overflow: hidden;
+  border-radius: 6px;
+  width: 160px;
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+
+  &::after {
+    position: absolute;
+    content: "\\25BC";
+    font-size: 12px;
+    line-height: 1;
+    right: 16px;
+    top: 50%;
+    margin-top: -6px;
+  }
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    padding: 10px;
+    cursor: pointer;
+    width: 100%;
+    border: none;
+    box-shadow: none;
+    background: transparent;
+    background-image: none;
+  }
+`
 
 const FilterByDepartment = ({ data, onChange }) => {
   let department = [...new Set(data.map(d => d.department))]
@@ -19,7 +59,7 @@ const FilterByDepartment = ({ data, onChange }) => {
 
 export default (props => (
   <div>
-    <div>Filter by department</div>
-    <div><FilterByDepartment {...props} /></div>
+    <Title>Filter by department</Title>
+    <Container><FilterByDepartment {...props} /></Container>
   </div>
 ))
